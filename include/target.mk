fc93d4cb477a53c9d5a5641f7018f5af243bb9f5
xiaoqingfengATGH <xiaoqingfengatgm@gmail.com>
2020-04-07 23:41:26 +0800

    update x86 makefile

--- include/target.mk
+++ include/target.mk
@@ -12,8 +12,7 @@ __target_inc=1
 # default device type
 DEVICE_TYPE?=router
 
-:<<!
-# Default packages - the really basic set
+# Default packages - the really basic set \
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd \
 block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw wget libustream-openssl ca-certificates \
 default-settings luci luci-app-ddns luci-app-upnp luci-app-adbyby-plus luci-app-autoreboot \
@@ -21,7 +20,6 @@ luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-unblockmusic \
 luci-app-arpbind luci-app-vlmcsd luci-app-wol luci-app-ramfree \
 luci-app-sfe luci-app-flowoffload luci-app-nlbwmon luci-app-accesscontrol luci-app-cpufreq \
 ddns-scripts_aliyun ddns-scripts_dnspod
-!
 
 # Home Lede default packages
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd \
